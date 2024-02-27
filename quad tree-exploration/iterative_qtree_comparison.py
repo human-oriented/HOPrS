1450e565c533bb570285f96cfe81afb2aa82bb41
Matthew Pendlebury <matthew@openorigins.com>
2024-02-27 08:56:07 +0000

    Changed format for encode_file_to_depth. Got iterative compare to ouput a purged qt Updated encode_all.sh

--- quad tree-exploration/iterative_qtree_comparison.py
+++ quad tree-exploration/iterative_qtree_comparison.py
@@ -1,11 +1,17 @@
 import cv2
 import sys
 import os
-
+from enum import Enum
 # Initialize debug_mode based on command line arguments
 debug_mode = '--debug' in sys.argv
 if debug_mode:
     sys.argv.remove('--debug')
+
+class Matched(Enum):
+    YES = 1
+    NO = 2
+    UNKNOWN = 3
+
 
 class TreeNode:
     def __init__(self, line, is_root=False):
@@ -16,17 +22,64 @@ class TreeNode:
         self.children = {}
         self.removed = False
         self.ham_distance = -1
+        self.matched = Matched.UNKNOWN
+        self.purge = False #Will be set to true later if all subnodes don't match
+
 
     def add_child(self, path_segment, child_node):
         self.children[path_segment] = child_node
 
+    def should_purge(self):
+        print(f"should_purge entered {str(self.path)} matched {self.matched}")
+        # Check if the current node should be purged
+        if self.matched != Matched.NO:
+            print(f"  1 Matched(green) - don't look further than this {str(self.path)} matched={self.matched} ")
+            return False
+        
+        #print(f"  3 red.  Number of children {len(self.children.values())} ")
+        # Recursively check if all children should be purged
+        for child in self.children.values():
+            #print(f"  4 this node {self.path} and iterated child: {child.path} ")
+            if not child.should_purge():
+                #print(f"  2 NO False child says it shouldn't purge: {str(child.path)} ")
+                return False
+            
+        # If the node and all its children should be purged,
+        # set purge to True and return True
+        self.purge = True
+        print(f"  Purging this node {str(self.path)} which has hamming {self.ham_distance}")
+        return True
+    
+    def purge_tree(self):
+        self.should_purge()
+        for child in self.children.values():
+            child.purge_tree()
+
+    def print_tree(self, file, unpurged_only):
+        
+        parts = self.line.split(',')
+        x0, y0, x1, y1 = map(int, parts[2:6])
+        quality = parts[10]
+        level = parts[1]
+        path = parts[0]
+        width, height = x1 - x0, y1 - y0
+        
+        if unpurged_only:            
+            if self.purge:
+                #explicitly stop recursion into sub elements although they /should/ all be purged=True
+                return
+        #print(f"print_tree Called with path {str(path)}")
+        
+        file.write(f"{path},{level},{x0},{y0},{x1},{y1},{width},{height},pdq,{self.hash},{quality}\n")
+
+        for child in self.children.values():
+            child.print_tree(file, unpurged_only)
+
+
+
 def parse_file_to_tree(filepath):
     with open(filepath, 'r') as f:
         first_line = next(f, None)
-        #discard first line which is details of the cropping or not
-        first_line = next(f, None)
-        
-        
         root = TreeNode(first_line.strip(), is_root=True) if first_line else None
         
         for line in f:
@@ -40,19 +93,20 @@ def parse_file_to_tree(filepath):
                     current = current.children[segment]
     return root
 
+
+
 def hamming_distance(hash1, hash2):
     b1 = bin(int(hash1, 16))[2:].zfill(256)
     b2 = bin(int(hash2, 16))[2:].zfill(256)
     return sum(c1 != c2 for c1, c2 in zip(b1, b2))
 
+#removed from the comparison - we've got a match, we don't need to go further.  
 def mark_as_removed(node):
     node.removed = True
     for child in node.children.values():
         mark_as_removed(child)
 
 def draw_comparison(image, node1, node2, output_path, counter):
-
-
     parts = node1.line.split(',')
     x0, y0, x1, y1 = map(int, parts[2:6])
     x = int(x0 + (x1 - x0) / 2)
@@ -77,11 +131,15 @@ def compare_and_output_images(image, node1, node2, image_path, output_path, thre
         distance = hamming_distance(node1.hash, node2.hash)
         node1.ham_distance = distance
         node2.ham_distance = distance
-        
+        print(f"DEBUG Comparing {distance} {node1.path} {node1.hash} {node2.hash}")
+
         if distance <= threshold:
-            mark_as_removed(node1)
+            mark_as_removed(node1)#removed from comparison because it (or a parent node) is matched - this is distinct from purged
             mark_as_removed(node2)
-        
+            node1.matched = Matched.YES
+        else:
+            node1.matched = Matched.NO
+                
         draw_comparison(image, node1, node2, output_path, counter)
         counter[0] += 1
     
@@ -89,29 +147,63 @@ def compare_and_output_images(image, node1, node2, image_path, output_path, thre
         if key in node2.children:
             compare_and_output_images(image, node1.children[key], node2.children[key], image_path, output_path, threshold, counter, compare_depth)
 
-def main(image_path: str, file1_path: str, file2_path: str, output_path: str, threshold: int, compare_depth: int):
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+def main(original_image: str, original_image_qt: str, new_image: str, new_image_qt: str, new_image_output_path: str, threshold:int, compare_depth:int):
     
-    tree1 = parse_file_to_tree(file1_path)
-    tree2 = parse_file_to_tree(file2_path)
+    if not os.path.exists(new_image_output_path):
+        os.makedirs(new_image_output_path)
     
-    image = cv2.imread(image_path) #caution of rotated images that cv2 doesn't handle
+    tree1 = parse_file_to_tree(original_image_qt)
+    tree2 = parse_file_to_tree(new_image_qt)
+    
+    image = cv2.imread(original_image) #caution of rotated images that cv2 doesn't handle
  
-    compare_and_output_images(image, tree1, tree2, image_path, output_path, threshold, [0], compare_depth)
+    compare_and_output_images(image, tree1, tree2, original_image, new_image_output_path, threshold, [0], compare_depth)
     
     # In non-debug mode, save the last image again to ensure the final state is preserved
     if not debug_mode:
-        draw_comparison(image, tree1, tree2, output_path, [-1])  # Use a unique counter to indicate the last image
+        draw_comparison(image, tree1, tree2, new_image_output_path, [-1])  # Use a unique counter to indicate the last image
+
+    tree1.purge_tree()#remove all the nodes that are unmatched AND all children are also unmatched
+
+    with open('tmp.purged.qt', 'w') as f:
+        tree1.print_tree(f, True) #write the newly purged tree to 
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 7:
-        print("Usage: python script.py image_path file1_path file2_path output_path threshold depth [--debug]")
-        print("-debug will emit a series of images that show the build up of the quad tree and comparision process")
+    if len(sys.argv) != 5 :
+        print("Usage: python iterative_qtree_comparison.py original_image new_image threshold depth [--debug]")
+        print("--debug will emit a series of images that show the build up of the quad tree and comparision process - may be a *lot* of images")
+        print("Expects a qt file with a .qt extension that is <image>.hoprs and will overwrite <new_image>.hoprs with a pruned tree")
         sys.exit(1)
+
+    original_image  = sys.argv[1]
+    original_image_qt = original_image + ".qt"
+    print(f"original image {original_image}")
+    print(f"original image QT {original_image_qt}")
     
-    image_path, file1_path, file2_path, output_path = sys.argv[1:5]
-    threshold = int(sys.argv[5])
-    compare_depth = int(sys.argv[6])
+    new_image  = sys.argv[2]
+    new_image_qt  = new_image + ".qt"
+    print(f"new image {new_image}")
+    print(f"new image QT {new_image_qt}")
     
-    main(image_path, file1_path, file2_path, output_path, threshold, compare_depth)
+    new_image_output_path = "output_" + new_image
+    print(f"new image output {new_image_output_path}")
+    
+    #Concatenate the two HOPRS files
+    # Open file1.txt and file2.txt in read mode, and file3.txt in write mode
+    with open(original_image + '.hoprs', 'r') as file1, open(new_image + '.hoprs', 'r') as file2, open(new_image_output_path + '.hoprs', 'w') as file3:
+        # Read and write the contents of file1 to file3
+        contents_of_file1 = file1.read()
+        file3.write(contents_of_file1)
+        
+        # Optionally, add a newline or separator if needed
+        file3.write('\n')  # This ensures there is a newline separating the contents of file1 and file2
+        # Read and write the contents of file2 to file3
+        contents_of_file2 = file2.read()
+        file3.write(contents_of_file2)
+
+    
+    threshold = int(sys.argv[3])
+    compare_depth = int(sys.argv[4])
+    
+    main(original_image, original_image_qt, new_image, new_image_qt, new_image_output_path, threshold, compare_depth)
