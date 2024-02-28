aab84c3194381484c4a90f33d3b13429aaa149b4
Matthew Pendlebury <matthew@openorigins.com>
2024-02-28 07:26:48 +0000

    Display a percentage of the image that is intact. Removed some debug output during processing

--- quad tree-exploration/iterative_qtree_comparison.py
+++ quad tree-exploration/iterative_qtree_comparison.py
@@ -4,6 +4,7 @@ import os
 from enum import Enum
 # Initialize debug_mode based on command line arguments
 debug_mode = '--debug' in sys.argv
+
 if debug_mode:
     sys.argv.remove('--debug')
 
@@ -25,29 +26,23 @@ class TreeNode:
         self.matched = Matched.UNKNOWN
         self.purge = False #Will be set to true later if all subnodes don't match
         self.optimise = False
+        
 
     def add_child(self, path_segment, child_node):
         self.children[path_segment] = child_node
 
     def should_purge(self):
-        print(f"should_purge entered {str(self.path)} matched {self.matched}")
         # Check if the current node should be purged
         if self.matched != Matched.NO:
-            print(f"  1 Matched(green) - don't look further than this {str(self.path)} matched={self.matched} ")
             return False
-        
-        #print(f"  3 red.  Number of children {len(self.children.values())} ")
         # Recursively check if all children should be purged
         for child in self.children.values():
-            #print(f"  4 this node {self.path} and iterated child: {child.path} ")
             if not child.should_purge():
-                #print(f"  2 NO False child says it shouldn't purge: {str(child.path)} ")
                 return False
 
         # If the node and all its children should be purged,
         # set purge to True and return True
         self.purge = True
-        print(f"  Purging this node {str(self.path)} which has hamming {self.ham_distance}")
         return True
     
     def purge_tree(self):
@@ -57,25 +52,19 @@ class TreeNode:
 
     #Call this after the purge.  It'll also reduce out the unknown state. 
     def should_optimise(self):
-        print(f"should_optimise entered {str(self.path)} matched {self.matched}")
         # Check if the current node should be purged
 
         if self.purge == False and (self.matched == Matched.YES or self.matched == Matched.NO):
-            print(f"  Can't optimise {str(self.path)} matched={self.matched} and purge(red)={self.purge}")
             return False
         
-        #print(f"  3 red.  Number of children {len(self.children.values())} ")
         # Recursively check if all children should be purged
         for child in self.children.values():
-            #print(f"  4 this node {self.path} and iterated child: {child.path} ")
             if not child.should_optimise():
-                print(f"  2 NO Not optimising this node : {str(child.path)} matched={self.matched} purge={self.purge}")
                 return False
             
         # If the node and all its children should be purged,
         # set purge to True and return True
         self.optimise = True
-        print(f"  Optimising this node {str(self.path)} ")
         return True
     
     def optimise_tree(self):
@@ -96,8 +85,7 @@ class TreeNode:
             if self.purge:
                 #explicitly stop recursion into sub elements although they /should/ all be purged=True
                 return
-        #print(f"print_tree Called with path {str(path)}")
-        
+    
         file.write(f"{path},{level},{x0},{y0},{x1},{y1},{width},{height},pdq,{self.hash},{quality}\n")
 
         for child in self.children.values():
@@ -114,14 +102,11 @@ class TreeNode:
         
         if self.optimise:
             #explicitly stop recursion into sub elements although they /should/ all be purged=True
-            return
-    #print(f"print_tree Called with path {str(path)}")
-        
+            return        
         file.write(f"{path},{level},{x0},{y0},{x1},{y1},{width},{height},pdq,{self.hash},{quality}\n")
 
         for child in self.children.values():
             child.print_optimised_tree(file)
-
 
 
 def parse_file_to_tree(filepath):
@@ -153,12 +138,14 @@ def mark_as_removed(node):
     for child in node.children.values():
         mark_as_removed(child)
 
-def draw_comparison(image, node1, node2, output_path, counter):
+def draw_comparison(image,list_pixel_counter, node1, node2, output_path, counter):
     parts = node1.line.split(',')
     x0, y0, x1, y1 = map(int, parts[2:6])
     x = int(x0 + (x1 - x0) / 2)
     y = int(y0 + (y1 - y0) / 2)
     
+    if (node1.removed):
+        list_pixel_counter[0] += (x1-x0) * (y1-y0) #counter of the matched pixels. 
     color = (0, 255, 0) if node1.removed else (0, 0, 255)
     cv2.rectangle(image, (x0, y0), (x1, y1), color, 4)
     cv2.putText(image, str(node1.ham_distance), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 2)
@@ -170,7 +157,7 @@ def draw_comparison(image, node1, node2, output_path, counter):
         return
     cv2.imwrite(f"{output_path}/comparison_{counter[0]:04}.jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
 
-def compare_and_output_images(image, node1, node2, image_path, output_path, threshold, counter=[0], compare_depth=99):
+def compare_and_output_images(image, list_pixel_counter, node1, node2, image_path, output_path, threshold, counter=[0], compare_depth=99):
     if len(node1.path.split('-')) - 1 >= compare_depth:
         return
     
@@ -178,7 +165,7 @@ def compare_and_output_images(image, node1, node2, image_path, output_path, thre
         distance = hamming_distance(node1.hash, node2.hash)
         node1.ham_distance = distance
         node2.ham_distance = distance
-        print(f"DEBUG Comparing {distance} {node1.path} {node1.hash} {node2.hash}")
+        #print(f"DEBUG Comparing {distance} {node1.path} {node1.hash} {node2.hash}")
 
         if distance <= threshold:
             mark_as_removed(node1)#removed from comparison because it (or a parent node) is matched - this is distinct from purged
@@ -187,12 +174,12 @@ def compare_and_output_images(image, node1, node2, image_path, output_path, thre
         else:
             node1.matched = Matched.NO
                 
-        draw_comparison(image, node1, node2, output_path, counter)
+        draw_comparison(image, list_pixel_counter, node1, node2, output_path, counter)
         counter[0] += 1
     
     for key in node1.children:
         if key in node2.children:
-            compare_and_output_images(image, node1.children[key], node2.children[key], image_path, output_path, threshold, counter, compare_depth)
+            compare_and_output_images(image, list_pixel_counter, node1.children[key], node2.children[key], image_path, output_path, threshold, counter, compare_depth)
 
 def main(original_image: str, original_image_qt: str, new_image: str, new_image_qt: str, new_image_output_path: str, threshold:int, compare_depth:int):
     
@@ -203,12 +190,13 @@ def main(original_image: str, original_image_qt: str, new_image: str, new_image_
     tree2 = parse_file_to_tree(new_image_qt)
     
     image = cv2.imread(original_image) #caution of rotated images that cv2 doesn't handle
- 
-    compare_and_output_images(image, tree1, tree2, original_image, new_image_output_path, threshold, [0], compare_depth)
+    pixel_counter = 0
+    list_pixel_counter = [pixel_counter]
+    compare_and_output_images(image, list_pixel_counter, tree1, tree2, original_image, new_image_output_path, threshold, [0], compare_depth)
     
     # In non-debug mode, save the last image again to ensure the final state is preserved
     if not debug_mode:
-        draw_comparison(image, tree1, tree2, new_image_output_path, [-1])  # Use a unique counter to indicate the last image
+        draw_comparison(image, list_pixel_counter, tree1, tree2, new_image_output_path, [-1])  # Use a unique counter to indicate the last image
 
     tree1.purge_tree()#remove all the nodes that are unmatched AND all children are also unmatched
     tree1.optimise_tree()#also create a tree that is a minimal node set. 
@@ -218,6 +206,14 @@ def main(original_image: str, original_image_qt: str, new_image: str, new_image_
 
     with open('tmp.optimised.qt', 'w') as f:
         tree1.print_optimised_tree(f) #write the newly purged tree to 
+
+
+    height, width = image.shape[:2]
+
+    image_pixels = width * height
+    proportion = list_pixel_counter[0]/image_pixels
+
+    print (f"Matched pixels {list_pixel_counter[0]} out of {image_pixels} which is {proportion:.2%}")
 
 
 
