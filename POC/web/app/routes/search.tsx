01b4f13bebbafc352907c121a2468a8f0e356f3b
Alex-OpenOrigins <97303133+Alex-OpenOrigins@users.noreply.github.com>
2024-08-27 10:59:51 +0100

    search route added

---
+++ POC/web/app/routes/search.tsx
@@ -0,0 +1,7 @@
+export default function Search () {
+    return(
+        <div className="p-5 container m-auto text-center items-center justify-center flex flex-col flex-stretch min-h-screen pt-20">
+            <h1>Search</h1>
+        </div>
+    )
+}
